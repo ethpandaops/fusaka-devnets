ce8f716962f4e90a03350ce8443c39a8d39b098f
parithosh <parithosh.jayanthi@ethereum.org>
2025-10-08 15:45:41 +0200

    update instances

--- terraform/sepsf-0/nodes.tf
+++ terraform/sepsf-0/nodes.tf
@@ -112,6 +112,15 @@ variable "lighthouse_reth_super" {
   }
 }
 
+variable "lighthouse_nimbusel_super" {
+  default = {
+    name            = "lighthouse-nimbusel-super"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
 variable "prysm_geth_super" {
   default = {
     name            = "prysm-geth-super"
@@ -142,6 +151,15 @@ variable "prysm_erigon_super" {
 variable "prysm_reth_super" {
   default = {
     name            = "prysm-reth-super"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "prysm_nimbusel_super" {
+  default = {
+    name            = "prysm-nimbusel-super"
     count           = 0
     validator_start = 0
     validator_end   = 0
@@ -184,6 +202,15 @@ variable "teku_reth_super" {
   }
 }
 
+variable "teku_nimbusel_super" {
+  default = {
+    name            = "teku-nimbusel-super"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
 variable "nimbus_geth_super" {
   default = {
     name            = "nimbus-geth-super"
@@ -214,6 +241,15 @@ variable "nimbus_besu_super" {
 variable "nimbus_reth_super" {
   default = {
     name            = "nimbus-reth-super"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_nimbusel_super" {
+  default = {
+    name            = "nimbus-nimbusel-super"
     count           = 0
     validator_start = 0
     validator_end   = 0
@@ -256,6 +292,15 @@ variable "lodestar_erigon_super" {
   }
 }
 
+variable "lodestar_nimbusel_super" {
+  default = {
+    name            = "lodestar-nimbusel-super"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
 variable "grandine_nethermind_super" {
   default = {
     name            = "grandine-nethermind-super"
@@ -292,7 +337,72 @@ variable "grandine_reth_super" {
   }
 }
 
+variable "grandine_nimbusel_super" {
+  default = {
+    name            = "grandine-nimbusel-super"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
 # Fullnodes
+# Lighthouse
+variable "lighthouse_geth_full" {
+  default = {
+    name            = "lighthouse-geth-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lighthouse_besu_full" {
+  default = {
+    name            = "lighthouse-besu-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lighthouse_nethermind_full" {
+  default = {
+    name            = "lighthouse-nethermind-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lighthouse_erigon_full" {
+  default = {
+    name            = "lighthouse-erigon-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lighthouse_reth_full" {
+  default = {
+    name            = "lighthouse-reth-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "lighthouse_nimbusel_full" {
+  default = {
+    name            = "lighthouse-nimbusel-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Prysm
 variable "prysm_geth_full" {
   default = {
     name            = "prysm-geth-full"
@@ -338,51 +448,16 @@ variable "prysm_reth_full" {
   }
 }
 
-variable "nimbus_geth_full" {
+variable "prysm_nimbusel_full" {
   default = {
-    name            = "nimbus-geth-full"
+    name            = "prysm-nimbusel-full"
     count           = 0
     validator_start = 0
     validator_end   = 0
   }
 }
 
-variable "nimbus_nethermind_full" {
-  default = {
-    name            = "nimbus-nethermind-full"
-    count           = 0
-    validator_start = 0
-    validator_end   = 0
-  }
-}
-
-variable "nimbus_besu_full" {
-  default = {
-    name            = "nimbus-besu-full"
-    count           = 0
-    validator_start = 0
-    validator_end   = 0
-  }
-}
-
-variable "nimbus_erigon_full" {
-  default = {
-    name            = "nimbus-erigon-full"
-    count           = 0
-    validator_start = 0
-    validator_end   = 0
-  }
-}
-
-variable "nimbus_reth_full" {
-  default = {
-    name            = "nimbus-reth-full"
-    count           = 0
-    validator_start = 0
-    validator_end   = 0
-  }
-}
-
+# Lodestar
 variable "lodestar_geth_full" {
   default = {
     name            = "lodestar-geth-full"
@@ -428,6 +503,126 @@ variable "lodestar_reth_full" {
   }
 }
 
+variable "lodestar_nimbusel_full" {
+  default = {
+    name            = "lodestar-nimbusel-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Nimbus
+variable "nimbus_geth_full" {
+  default = {
+    name            = "nimbus-geth-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_nethermind_full" {
+  default = {
+    name            = "nimbus-nethermind-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_besu_full" {
+  default = {
+    name            = "nimbus-besu-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_erigon_full" {
+  default = {
+    name            = "nimbus-erigon-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_reth_full" {
+  default = {
+    name            = "nimbus-reth-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "nimbus_nimbusel_full" {
+  default = {
+    name            = "nimbus-nimbusel-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Teku
+variable "teku_geth_full" {
+  default = {
+    name            = "teku-geth-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "teku_besu_full" {
+  default = {
+    name            = "teku-besu-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "teku_nethermind_full" {
+  default = {
+    name            = "teku-nethermind-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "teku_erigon_full" {
+  default = {
+    name            = "teku-erigon-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "teku_reth_full" {
+  default = {
+    name            = "teku-reth-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "teku_nimbusel_full" {
+  default = {
+    name            = "teku-nimbusel-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+# Grandine
 variable "grandine_geth_full" {
   default = {
     name            = "grandine-geth-full"
@@ -467,6 +662,15 @@ variable "grandine_erigon_full" {
 variable "grandine_reth_full" {
   default = {
     name            = "grandine-reth-full"
+    count           = 0
+    validator_start = 0
+    validator_end   = 0
+  }
+}
+
+variable "grandine_nimbusel_full" {
+  default = {
+    name            = "grandine-nimbusel-full"
     count           = 0
     validator_start = 0
     validator_end   = 0
